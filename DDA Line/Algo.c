176d0ff771ac0b910b3a81415776ec1be44a5bd8
Khairul Islam <48696824+KhairuI@users.noreply.github.com>
2020-11-17 20:53:11 +0600

    DDA Line Algorithm

---
+++ DDA Line/Algo.c
@@ -0,0 +1,73 @@
+#include <windows.h>
+#include <GL/glut.h>
+#include <stdlib.h>
+
+int   x1, x2, y1, y2, dx, dy;
+float ix, iy,step;
+void display(void)
+{
+glClear (GL_COLOR_BUFFER_BIT);
+glColor3b(240,108,23);
+dx = abs(x2-x1);
+dy = abs(y2-y1);
+if(abs(dx) > abs(dy) ){
+        step = abs(dx);
+    }
+    else
+    {
+        step = abs(dy);
+    }
+    ix = dx/step;
+    iy = dy/step;
+    float x=x1, y=y1;
+    glBegin(GL_POINTS);
+
+    glVertex2i (abs(x), abs(y));
+    //glEnd(); Can't use glEnd Here otherwise it will not display the next looping poinr
+
+    int i;
+    for (i = 0; i<step; i++)
+    {
+        x = x + ix;
+        y = y + iy;
+        printf("%.1f %.1f %.1f %.1f %.1f", x, y, step, ix, iy);
+        printf("\n");
+        glVertex2i (abs(x), abs(y));
+        // then again here you can't use glEnd() here otherwise it will stop drawing after one loop
+    }
+glEnd();
+glutSwapBuffers();
+}
+void init (void)
+{
+//glClearColor (0.0, 0.0, 0.0, 0.0);   /* select clearing (background) color */
+glClearColor (1, 1, 1, 0);
+glMatrixMode(GL_PROJECTION);        /* initialize viewing values */
+glLoadIdentity();
+//glOrtho(0.0, 500.0, 0.0, 500.0, -500.0, 500.0);
+gluOrtho2D(0,600,0,600);
+}
+int main(int argc, char** argv)
+{
+    printf(" Enter Value of x1: ");
+    scanf("%d", &x1);
+     printf(" Enter Value of y1: ");
+    scanf("%d", &y1);
+     printf(" Enter Value of x2: ");
+    scanf("%d", &x2);
+    printf(" Enter Value of y1: ");
+    scanf("%d", &y2);
+
+
+
+
+glutInit(&argc, argv);
+glutInitDisplayMode (GLUT_DOUBLE | GLUT_RGB);
+glutInitWindowSize (300, 300);
+glutInitWindowPosition (100, 100);
+glutCreateWindow ("172-15-9838");
+init ();
+glutDisplayFunc(display);
+glutMainLoop();
+return 0; /* ISO C requires main to return int. */
+}
